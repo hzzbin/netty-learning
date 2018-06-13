455836c5da69c213de2e7ebce164ea3bf65cf49e
张斌(焦点.213309) <binzhang213309@sohu-inc.com>
2018-06-13 09:44:36 +0800

    add netty_example

---
+++ src/NettyLearningExample/Echo/EchoServerHandler.java
@@ -0,0 +1,32 @@
+package NettyLearningExample.Echo;
+
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf buf = (ByteBuf) msg;
+        System.out.println("Server received: " + ByteBufUtil.hexDump(buf.readBytes(buf.readableBytes())));
+
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+                .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/NettyLearningExample/test.java
@@ -0,0 +1,7 @@
+package NettyLearningExample;
+
+/**
+ * Created by binzhang213309 on 2018/6/11.
+ */
+public class test {
+}
